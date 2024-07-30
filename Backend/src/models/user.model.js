2be7e2010f70307fb2cd35b253da2fc263025cc8
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2024-07-30 23:19:12 +0530

    User register model

---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,52 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, // use cloudnary service
+      required: true,
+    },
+    coverimage: {
+      type: String, // use cloudnary service
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamp: true }
+);
+
+export const User = mongoose.model("User", userSchema);
