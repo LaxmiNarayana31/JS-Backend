d0d8ddb1e58faf80c6760fe0536b93099b79fc7c
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2024-08-02 23:57:32 +0530

    index file

---
+++ Backend/src/index.js
@@ -0,0 +1,41 @@
+// require('dotenv').config({path: './env'}) -> It works but not maintain consistency
+
+import dotenv from "dotenv";
+import connectDB from "./db/connection.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port:${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection failed");
+  });
+
+  
+// "IIFE" stands for "Immediately Invoked Function Expression". It's a way to execute a function as soon as it's defined in JavaScript.
+// Approach-1
+
+/*
+
+(async () => {
+  try {
+      await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+      app.on("error", (error) => {
+          console.error("ERROR:", error);
+          throw error
+      })
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`)
+    })
+  } catch (error) {
+    console.error("ERROR:", error);
+  }
+})();
+
+*/
