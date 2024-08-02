a30caff05080f85277bc3bd529d54a158431c832
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2024-08-02 23:56:39 +0530

    async handling

---
+++ Backend/src/utils/asyncHandler.js
@@ -0,0 +1,28 @@
+const asyncHandler = (requestHandler) => {
+  (req, resp, next) => {
+    Promise.resolve(requestHandler(req, resp, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+// const asyncHandler = () => { }
+// const asyncHandler = (func) => () => { }
+// const asyncHandler = (func) => async () => { }
+
+/*
+
+// production codebases
+
+const asyncHandler = (fn) => async (req, resp, next) => {
+    try {
+        await fn(req, resp, next)
+    } catch (error) {
+        resp.status(err.code || 500).json({
+            success: true,
+            message: err.message
+        })
+    }
+}
+
+*/
