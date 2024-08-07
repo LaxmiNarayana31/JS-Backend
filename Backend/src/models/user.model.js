dafaf55d19f7d6cf0178a7a96781d95d00405c5a
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2024-08-07 08:16:30 +0530

    user model and generate access token

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -49,4 +49,38 @@ const userSchema = new Schema(
   { timestamp: true }
 );
 
+// Encrypt the password using bcrypt
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = bcrypt.hash(this.password, 10)
+  next()
+})
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password)
+}
+
+
+// Generate access token 
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname
+    }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+}
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    }, process.env.JWT_SECRET, {
+    expiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN,
+  });
+}
+
 export const User = mongoose.model("User", userSchema);
