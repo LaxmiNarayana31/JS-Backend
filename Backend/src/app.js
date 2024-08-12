4cd8e1833ddfdeef5ffebd0c054921dc0e3192d6
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2024-08-12 23:52:33 +0530

    express app file

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -23,10 +23,6 @@ app.use("/api/v1/user", userRouter);
 
 export { app };
 
-  
-  
-  
-  
 // routes declaration
 // you can't directly write app.get() -> in that all you write all routes and controllers in a single file
 // in this case all routes are write in a separate folder, so it is mandotory to use middlewares -> for that use app.use
