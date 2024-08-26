b95aff7aeb1f35f4d8361fd99862b61f90b3b440
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2024-08-27 00:03:59 +0530

    multer middleware

---
+++ Backend/src/middlewares/multer.middleware.js
@@ -0,0 +1,13 @@
+import multer from "multer";
+
+// https://github.com/expressjs/multer/blob/master/README.md
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/temp");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname); // cb -> callback
+  },
+});
+
+export const upload = multer({ storage: storage });
