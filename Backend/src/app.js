e3459633f1a1588f7fd100a566ff22fe63c8f793
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2024-09-01 09:36:22 +0530

    integrate swagger docs

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import { swaggerUi, swaggerDocument } from "./swagger/swaggerConfig.js"; // Import Swagger
 
 const app = express();
 
@@ -15,6 +16,9 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+// Serve Swagger UI at the /api-docs endpoint
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // routes import
 import userRouter from "./routes/user.routes.js";
